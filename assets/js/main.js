c47f69832e9fb2e08cf7fb131b3a17893ea82b53
abir-ahmmed <abirahmmedrana@outlook.com>
2022-12-01 21:41:42 +0600

    contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,13 +30,57 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".projects_container", {
+    loop: true,
+    spaceBetween:24,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+      },
+  });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial_container", {
+  grabCursor: true,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+      e.preventDefault()
+      // Check if the field has a value
+      if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+        	// Add and remove color
+          contactMessage.classList.remove('color-blue')
+          contactMessage.classList.add('color-red')
+
+          	// Show message	
+            contactMessage.textContent = 'Write all the input fields '
+      }
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
